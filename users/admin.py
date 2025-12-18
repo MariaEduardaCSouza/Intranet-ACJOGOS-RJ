e5bfa557201dad08ebda5eb04890fbe59420ae2d
Maria Eduarda <223533892+M4rib4ck@users.noreply.github.com>
2025-12-18 15:32:25 -0300

    feat(users): register custom user model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    '''
+    Configuração do admin para o modelo User.
+    Exibe e permite a edição dos campos principais do usuário.
+    '''
+    list_display = ('email','nome', 'tipo_usuario', 'is_active', 'is_staff')
+    search_fields = ('email', 'nome', 'cpf')
+    list_filter = ('tipo_usuario', 'is_active', 'is_staff')
