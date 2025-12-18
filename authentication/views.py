423f3fc5247f9c7f7da726309ddca78bd398bc3a
Maria Eduarda <223533892+M4rib4ck@users.noreply.github.com>
2025-12-18 15:41:47 -0300

    feat(auth): create backend login view using email authentication

---
+++ authentication/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+def login_view(request):
+    '''
+    view responsável por autenticar usuários na plataforma via e-mail e senha.
+    '''
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')  #Ajustar depois para a página inicial correta
+        else:
+            context = {'error': 'E-mail ou senha inválidos.'}
+            return render(request, 'authentication/login.html', context)
+        
