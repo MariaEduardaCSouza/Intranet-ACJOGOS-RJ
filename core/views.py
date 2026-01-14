510a0396ab7f16c6aaac3f9a65b39e363d6ecab3
Maria Eduarda <223533892+M4rib4ck@users.noreply.github.com>
2026-01-14 14:01:30 -0300

    feat(user): Adjust layout of home view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.permissions import require_diretoria
 
 @login_required
 def home(request):
@@ -13,3 +14,7 @@ def home(request):
     }
     
     return render(request, 'home.html')
+
+def area_diretoria(request):
+    require_diretoria(request.user)
+    return render(request, 'diretoria.html')
